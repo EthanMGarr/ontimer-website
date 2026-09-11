c810ee10bb10185f579282fd4deb2b04db105bec
Ethan <ethanmgarr@gmail.com>
2026-09-11 18:41:17 -0400

    Restore current app icon in homepage header

--- src/components/Homepage2Header.tsx
+++ src/components/Homepage2Header.tsx
@@ -51,7 +51,7 @@ export function Homepage2Header() {
     <header className="hp2-header">
       <div className="hp2-header__bar">
         <Link href="/" className="hp2-brand" aria-label="OnTimer home">
-          <Image src="/images/homepage2/ontimer-icon-blue.png" alt="" width={40} height={40} priority />
+          <Image src="/images/ontimer_1024x1024.png" alt="" width={40} height={40} priority />
           <span>OnTimer</span>
         </Link>
         <nav className="hp2-nav" aria-label="Main navigation">
